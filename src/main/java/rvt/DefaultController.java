c561d6cd963142dc4ed3fffde9b24acd0d5faf6a
22DP3KBori <144322401+22DP3KBori@users.noreply.github.com>
2024-02-22 13:49:30 +0000

    Logs register

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -1,9 +1,12 @@
 package rvt;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class DefaultController {
@@ -11,5 +14,10 @@ public class DefaultController {
    @GetMapping(value = "/")
     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
         return "index";
+    }
+    @GetMapping(value = "/register")
+    ModelAndView register() {
+        ModelAndView modelAndView = new ModelAndView("register");
+        return modelAndView;
     }
 }
